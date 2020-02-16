dcdc4872986317261893aff45ecf2189f40b086a
Vicky Wu <vicky.wu@theskale.com>
2020-02-15 19:42:31 -0800

    Add colors file to have all the colors in one place, and added view and text components to app.js

---
+++ src/Colors.js
@@ -0,0 +1,56 @@
+// air colors
+
+const airBlack = "#101820"; // Pantone Black 6 C
+const airBlue = "#307FE2"; // Pantone 2727 C
+const airDarkGray = "#9EA2A2"; // Pantone 422 C
+const airGray = "#C7C9C7"; // Pantone 420 C
+const airLightBlue = "#59CBE8"; // Pantone 305 C
+const airLightGray = "#D9D9D6"; // Pantone Cool Gray 1 C
+const airLinkBlue = "#3C74AA"; // Pantone 7689 C
+const airMainBlue = "#1D428A"; // Pantone 7687 C
+const airRed = "#FF585D"; // Pantone 178 C
+const airTeal = "#62A8C2"; // Pantone 3265 C
+const airWhite = "#FDFDFD"; // Off-white (not Pantone)
+const airYellow = "#FFC845"; // Pantone 1225 C
+const airGreen = "#27AE60";
+
+const airBorderGray = "#BDBDBD";
+const airFillGray = "#E0E0E0";
+const airFillLightGray = "#F2F2F2";
+const airFillStormy = "#AFB7C2";
+const airTextGray = "#4F4F4F";
+const airTextLightGray = "#828282";
+const airBlueGray = "#617383";
+
+const airCellRed = "#FCE5CD";
+const airCellYellow = "#FFF2CC";
+const airCellGreen = "#C9DFC5";
+
+const airCircleLightBlue = "#56CCF2";
+
+export {
+  airBlack,
+  airBlue,
+  airDarkGray,
+  airGray,
+  airLightBlue,
+  airLightGray,
+  airLinkBlue,
+  airMainBlue,
+  airRed,
+  airTeal,
+  airWhite,
+  airYellow,
+  airGreen,
+  airBorderGray,
+  airFillGray,
+  airFillLightGray,
+  airFillStormy,
+  airTextGray,
+  airTextLightGray,
+  airBlueGray,
+  airCellRed,
+  airCellYellow,
+  airCellGreen,
+  airCircleLightBlue
+};
