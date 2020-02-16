f1a80f67ccc4ece8ea070c7f9ef952bb450392e8
Jessica Bainbridge-Smith <publicjbs@gmail.com>
2020-02-16 09:32:29 -0800

    Implemented post request on tooltip

--- src/Tooltip.js
+++ src/Tooltip.js
@@ -1,23 +1,48 @@
 import React from "react";
 import styled from "styled-components";
 
-function Tooltip(props) {
-  return (
-    <div>
-      <LeadIn>I think I'm a</LeadIn>
-      <form onSubmit={props.post}> 
-        <label>
-          Talker
-          <input type="radio" />
-        </label>
-        <label>
-          Non-talker
-          <input type="radio" />
-        </label>
-        <button>Sit here</button>
-      </form>
-    </div>
-  );
+class Tooltip extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      talker: false,
+      stoic: false
+    };
+  }
+
+  onChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    this.setState({
+      [name]: value
+    })
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    const likesToTalk = this.state.talker === "on" ? true : false;
+    console.log(likesToTalk);
+    this.props.post(this.props.number, likesToTalk);
+  }
+
+  render = () => {
+    return (
+      <div>
+        <LeadIn>I think I'm a</LeadIn>
+        <form onSubmit={this.onSubmit}> 
+          <label>
+            Talker
+            <input type="radio" name="talker" onChange={this.onChange}/>
+          </label>
+          <label>
+            Non-talker
+            <input type="radio" name="stoic" onChange={this.onChange}/>
+          </label>
+          <button>Sit here</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Tooltip;
