5d3ecae630ae6049c67c9fb59e4f057af23490bc
David Philipson <david.philipson@gmail.com>
2020-02-16 01:51:47 -0800

    available to isAvailable

--- src/SeatMap.js
+++ src/SeatMap.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 // import seatMap from './seatmap.json';
-import Seat from './Seat.js';
+import Seat from "./Seat.js";
 
 const Container = styled.div`
-height: auto;
-width: 350px;
-margin: 0 auto;
-padding: 4em 0;
-border-radius: 15px;
-background-color: #F5F5F5;
-display: grid;
-grid-template-columns: repeat(${props => props.width}, 50px);
-grid-auto-rows: 50px;
-grid-row-gap: 10px;
-justify-items: center;
-justify-content: center;
+  height: auto;
+  width: 350px;
+  margin: 0 auto;
+  padding: 4em 0;
+  border-radius: 15px;
+  background-color: #f5f5f5;
+  display: grid;
+  grid-template-columns: repeat(${props => props.width}, 50px);
+  grid-auto-rows: 50px;
+  grid-row-gap: 10px;
+  justify-items: center;
+  justify-content: center;
 `;
 
 const Item = styled.div`
@@ -36,7 +36,7 @@ class SeatMap extends React.Component {
 
   onSubmit(event) {
     event.preventDefault();
-    alert('submitted!');
+    alert("submitted!");
   }
 
   render() {
@@ -44,15 +44,18 @@ class SeatMap extends React.Component {
     return (
       <Main>
         <Container width={data.width}>
-        <Item col={0}>A</Item>
-        <Item col={1}>B</Item>
-        <Item col={3}>C</Item>
-        <Item col={4}>D</Item>
-          {data.seats.map(seat => 
+          <Item col={0}>A</Item>
+          <Item col={1}>B</Item>
+          <Item col={3}>C</Item>
+          <Item col={4}>D</Item>
+          {data.seats.map(seat => (
             <Item key={seat.number} col={seat.x}>
-                <Seat preferences={seat.preferences} available={seat.available} />
+              <Seat
+                preferences={seat.preferences}
+                available={seat.isAvailable}
+              />
             </Item>
-          )}
+          ))}
         </Container>
       </Main>
     );
