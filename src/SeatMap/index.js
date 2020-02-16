1956a4c5dad77f7b72e0818703bb0de4b5732a2a
Desmond Rogers <desmondrogers2@gmail.com>
2020-02-15 19:12:14 -0800

    Add bootstrap

---
+++ src/SeatMap/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import seatMap from './seatmap.json';
+
+// api call to backend endpoint
+
+class SeatMap extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const { seats } = seatMap;
+
+    return (
+    )
+  }
+}
+
+export default SeatMap;
