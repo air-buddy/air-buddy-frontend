96f89abe3edc358df45b08a450da3cbcf979b70a
David Philipson <david.philipson@gmail.com>
2020-02-16 09:54:09 -0800

    Fix radio button behavior

--- src/Tooltip.js
+++ src/Tooltip.js
@@ -10,45 +10,61 @@ class Tooltip extends React.Component {
     };
   }
 
-  onChange = (event) => {
+  onChange = event => {
     const name = event.target.name;
     const value = event.target.value;
     this.setState({
+      talker: false,
+      stoic: false,
       [name]: value
-    })
-  }
+    });
+  };
 
-  onSubmit = (event) => {
+  onSubmit = event => {
     event.preventDefault();
     const likesToTalk = this.state.talker === "on" ? true : false;
     console.log(likesToTalk);
     this.props.post(this.props.number, likesToTalk);
-  }
+  };
 
   render = () => {
     return (
       <div>
         <LeadIn>I think I'm a</LeadIn>
-        <form onSubmit={this.onSubmit}> 
-          <label>
+        <form onSubmit={this.onSubmit}>
+          <Option>
             Talker
-            <input type="radio" name="talker" onChange={this.onChange}/>
-          </label>
-          <label>
+            <input
+              type="radio"
+              name="talker"
+              onChange={this.onChange}
+              checked={this.state.talker}
+            />
+          </Option>
+          <Option>
             Non-talker
-            <input type="radio" name="stoic" onChange={this.onChange}/>
-          </label>
+            <input
+              type="radio"
+              name="stoic"
+              onChange={this.onChange}
+              checked={this.state.stoic}
+            />
+          </Option>
           <button>Sit here</button>
         </form>
       </div>
     );
-  }
+  };
 }
 
 export default Tooltip;
 
+const Option = styled.label`
+  margin-right: 10px;
+`;
+
 const LeadIn = styled.p`
   margin-top: 0;
   font-weight: bold;
-  color: #3C74AA;
+  color: #3c74aa;
 `;
